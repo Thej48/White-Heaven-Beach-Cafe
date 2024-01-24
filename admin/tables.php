b4b9c3ed1f14d9a9f668c8dea7fc9c51862923c7
Thej48 <chattangadathimmaiah@gmail.com>
2024-01-24 09:49:30 +0530

    seventh commit : working on food-category section in admin panel

--- admin/tables.php
+++ admin/tables.php
@@ -117,119 +117,162 @@ if (!isset($_SESSION['authAdmin'])) {
                     <div class="tableCardsDiv overflow-auto py-2 px-3 d-flex flex-wrap row row-cols-auto ">
                         <?php
                         while ($row = $getTableRes->fetch_assoc()) {
-                            echo "
-                        <div class='card p-3 shadow-sm m-2 rounded-1 col tableCard'>
+                            if ($row['table_status'] == 0) { ?>
+                                <div class='card bg-white border border-secondary-subtle p-3 shadow-sm m-2 rounded-1 col tableCard' id="<?php echo $row['id']; ?>">
+                                <?php } elseif ($row['table_status'] == 1) { ?>
+                                    <div class='card bg-info bg-opacity-10 border border-secondary-subtle p-3 shadow-sm m-2 rounded-1 col tableCard' id="<?php echo $row['id']; ?>">
+                                    <?php }
+                                echo "
                             <h1 class='h3 fw-bold p-0 m-0'>Table " . $row['table_number'] . "</h1>
                             <hr class='my-2'/>
                             <img src='data:image;base64," . $row['table_qr'] . "' alt='qr-image' class='tableQrImg'>
                             <hr class='my-2'/>
                             ";
-                            if ($row['table_status'] == 0) {
-                        ?>
-                                <div class='tableStatusDiv d-flex align-items-center justify-content-between'>
-                                    <span class="fw-bold">Status : <span class="fw-medium">Available</span></span>
-                                    <img class="tableStatusChangeBtn" src='../icons/toggle-off.svg' alt='table-available'>
+                                if ($row['table_status'] == 0) {
+                                    ?>
+                                        <div class='tableStatusDiv d-flex align-items-center justify-content-between'>
+                                            <span class="fw-bold">Status : <span class="fw-medium">Available</span></span>
+                                            <img class="tableStatusChangeBtn" src='../icons/toggle-off.svg' onclick="document.location.href='./tables.php?reserveId=<?php echo $row['id']; ?>'" alt='table-available'>
+                                        </div>
+                                    <?php } elseif ($row['table_status'] == 1) { ?>
+                                        <div class='tableStatusDiv d-flex align-items-center justify-content-between'>
+                                            <span class="fw-bold">Status : <span class="fw-medium">Occupied</span></span>
+                                            <img class="tableStatusChangeBtn" src='../icons/toggle-on.svg' onclick="document.location.href='./tables.php?freeId=<?php echo $row['id']; ?>'" alt='table-occupied'>
+                                        </div>
+                                    <?php } ?>
+                                    </div>
+                                <?php } ?>
                                 </div>
-                            <?php } elseif ($row['table_status'] == 1) { ?>
-                                <div class='tableStatusDiv d-flex align-items-center justify-content-between'>
-                                    <span class="fw-bold">Status : <span class="fw-medium">Occupied</span></span>
-                                    <img class="tableStatusChangeBtn" src='../icons/toggle-on.svg' alt='table-available'>
-                                </div>
-                            <?php } ?>
-                    </div>
-                <?php } ?>
-            </div>
-        <?php
-                } else {
-                    echo "
+                            <?php
+                        } else {
+                            ?>
+                                <script>
+                                    var disableSearchInput = document.getElementById('tableSearchBox');
+                                    disableSearchInput.disabled = true;
+                                </script>
+                            <?php
+                            echo "
                     <div class='tableCardsDiv overflow-auto py-2 px-3 d-flex flex-column h-100 align-items-center justify-content-center'>
                         <h1 class='h2 fw-bolder text-decoration-underline'>Nothing to Show</h1>
                         <p class='h6 w-50 lh-base text-center'>We're sorry, but it looks like there are no details available for the tables at the moment. It's possible that there is no data to display. Please check back later.</p>
                     </div>
                 ";
-                }
-        ?>
+                        }
+                            ?>
 
-        <div class='tableCardsDivMsg overflow-auto py-2 px-3 flex-column h-100 align-items-center justify-content-center'>
-            <h1 class='h2 fw-bolder text-decoration-underline'>Nothing to Show</h1>
-            <p class='h6 w-50 lh-base text-center'>We're sorry, but it looks like there are no details available for the tables at the moment. It's possible that there is no data to display. Please check back later.</p>
+                            <div class='tableCardsDivMsg overflow-auto py-2 px-3 flex-column h-100 align-items-center justify-content-center'>
+                                <h1 class='h2 fw-bolder text-decoration-underline'>Nothing to Show</h1>
+                                <p class='h6 w-50 lh-base text-center'>We're sorry, but it looks like there are no details available for the tables at the moment. It's possible that there is no data to display. Please check back later.</p>
+                            </div>
+
+                    </div>
+
+            </div>
+
         </div>
 
-        </div>
-
-    </div>
-
-    </div>
-
-    <!-- SEARCH BAR SCRIPT -->
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const tableSearchBox = document.getElementById('tableSearchBox');
-            const tableCardsDiv = document.querySelector('.tableCardsDiv');
-            const tableCardsDivMsg = document.querySelector('.tableCardsDivMsg');
-
-            tableSearchBox.addEventListener('input', function() {
-                const searchValue = tableSearchBox.value.trim().toLowerCase();
-                const cards = document.querySelectorAll('.tableCard');
-
-                let matchFound = false;
-
-                cards.forEach(function(card) {
-                    const cardText = card.innerText.toLowerCase();
-
-                    if (cardText.includes(searchValue)) {
-                        card.style.display = 'block';
-                        matchFound = true;
-                    } else {
-                        card.style.display = 'none';
-                    }
-                });
-
-                if (matchFound) {
-                    tableCardsDivMsg.style.display = 'none';
-                } else {
-                    tableCardsDivMsg.style.display = 'flex';
-                }
-
-            });
-        });
-    </script>
-
-
-    <!-- ADD NEW TABLE -->
-    <script>
-        function addTable() {
-            // Open Add Table Modal
-            addTableModal.showModal();
-
+        <!-- SCRIPT TO UPDATE TABLE_STATUS -->
+        <script>
+            // Change table_status to 1
             <?php
-            $checkTableCount = "SELECT * FROM tables ORDER BY table_number ASC";
-            $query = mysqli_query($conn, $checkTableCount);
-            $res = mysqli_num_rows($query);
-            if ($res == 0) {
-                $tablenumber = 1;
-            } else {
-                // Move the internal result pointer to the last row
-                mysqli_data_seek($query, $res - 1);
-                $row = mysqli_fetch_array($query);
-                $newtablenumber = $row['table_number'];
-                $tablenumber = $newtablenumber + 1;
+            if (isset($_GET['reserveId'])) {
+                $tableResId = $_GET['reserveId'];
+                $reserveTableQuery = "UPDATE tables SET table_status = '1' WHERE id=$tableResId ";
+                $reserveTableRun = mysqli_query($conn, $reserveTableQuery);
+                if ($reserveTableRun) {
+            ?>
+                    window.location.href = './tables.php#<?php echo $tableResId ?>';
+                <?php
+                }
+                // Change table_status to 0
+            } elseif (isset($_GET['freeId'])) {
+                ?>
+                <?php
+                $tableFreeId = $_GET['freeId'];
+                $freeTableQuery = "UPDATE tables SET table_status = '0' WHERE id=$tableFreeId ";
+                $freeTableRun = mysqli_query($conn, $freeTableQuery);
+                if ($freeTableRun) {
+                ?>
+                    window.location.href = './tables.php#<?php echo $tableFreeId ?>';
+            <?php
+                }
             }
             ?>
-        }
+        </script>
+
+        <!-- SEARCH BAR SCRIPT -->
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const tableSearchBox = document.getElementById('tableSearchBox');
+                const tableCardsDiv = document.querySelector('.tableCardsDiv');
+                const tableCardsDivMsg = document.querySelector('.tableCardsDivMsg');
+
+                tableSearchBox.addEventListener('input', function() {
+                    const searchValue = tableSearchBox.value.trim().toLowerCase();
+                    const cards = document.querySelectorAll('.tableCard');
+
+                    let matchFound = false;
+
+                    cards.forEach(function(card) {
+                        const cardText = card.innerText.toLowerCase();
+
+                        if (cardText.includes(searchValue)) {
+                            card.style.display = 'block';
+                            matchFound = true;
+                        } else {
+                            card.style.display = 'none';
+                        }
+                    });
+
+                    if (matchFound) {
+                        tableCardsDivMsg.style.display = 'none';
+                    } else {
+                        tableCardsDivMsg.style.display = 'flex';
+                    }
+
+                });
+            });
+        </script>
 
 
-        function generateQRCode() {
-            window.location.href = "<?php echo "./generateQR.php?tableNo=$tablenumber" ?>";
-        }
-    </script>
+        <!-- ADD NEW TABLE -->
+        <script>
+            function addTable() {
+                // Open Add Table Modal
+                addTableModal.showModal();
 
-    <!-- ADD TABLE MODAL -->
-    <dialog class="addTableModal" id="addTableModal">
-        <h1>Do you want to add <span>Table - <?php echo "$tablenumber"; ?></span></h1>
-        <input type="submit" value="Yes" onclick="generateQRCode()">
-        <input type="submit" value="No" onclick="addTableModal.close()">
-    </dialog>
+                <?php
+                $checkTableCount = "SELECT * FROM tables ORDER BY table_number ASC";
+                $query = mysqli_query($conn, $checkTableCount);
+                $res = mysqli_num_rows($query);
+                if ($res == 0) {
+                    $tablenumber = 1;
+                } else {
+                    // Move the internal result pointer to the last row
+                    mysqli_data_seek($query, $res - 1);
+                    $row = mysqli_fetch_array($query);
+                    $newtablenumber = $row['table_number'];
+                    $tablenumber = $newtablenumber + 1;
+                }
+                ?>
+            }
+
+
+            function generateQRCode() {
+                window.location.href = "<?php echo "./generateQR.php?tableNo=$tablenumber" ?>";
+            }
+        </script>
+
+        <!-- ADD TABLE MODAL -->
+        <dialog class="addTableModal rounded" id="addTableModal">
+            <div class="addTableModalContentDiv row row-gap-2 d-flex justify-content-center h-100">
+                <h1 class="text-center display-5 fw-medium align-self-center">Do you want to add <span class="fw-bold">Table <?php echo "$tablenumber"; ?> ?</span></h1>
+                <div class="col d-flex gap-4 align-items-center justify-content-center">
+                    <input type="submit" class="addTableConfirmBtn col-sm-5 p-2 fs-4 rounded" value="Yes" onclick="generateQRCode()">
+                    <input type="submit" class="addTableCancelBtn col-sm-5 p-2 fs-4 rounded" value="No" onclick="addTableModal.close()">
+                </div>
+            </div>
+        </dialog>
 
 </body>
 
