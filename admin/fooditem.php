23e8f27c9146d22ae1a6ad5f096814b20e8bb226
Thej48 <chattangadathimmaiah@gmail.com>
2024-01-27 16:48:30 +0530

    15th commit : finishing working on admin panel

--- admin/fooditem.php
+++ admin/fooditem.php
@@ -146,8 +146,8 @@ if (!isset($_SESSION['authAdmin'])) {
                                                 <?php } ?>
                                                 <hr class="w-100 my-1">
                                                 <div class="FoodControlBtnDiv d-flex gap-2">
-                                                    <input type="submit" value="Edit" class="FoodEditBtn w-50 rounded py-1 fw-medium" id="FoodEditBtn">
-                                                    <input type="submit" value="Delete" class="FoodDeleteBtn bg-secondary-subtle w-50 rounded py-1 fw-bold" id="FoodDeleteBtn">
+                                                    <?php echo " <input type='submit' value='Edit' class='FoodEditBtn w-50 rounded py-1 fw-medium' id='FoodEditBtn' onClick='openEditFoodModal(\"" . $row['food_image'] . "\", \"" . $row['food_name'] . "\", \"" . $row['id'] . "\", \"" . $row['category_name'] . "\", \"" . $row['quantity'] . "\", \"" . $row['price'] . "\")'>
+                                                    <input type='submit' value='Delete' class='FoodDeleteBtn bg-secondary-subtle w-50 rounded py-1 fw-bold' id='FoodDeleteBtn' onClick='openDeleteFoodModal(\"" . $row['food_image'] . "\", \"" . $row['food_name'] . "\", \"" . $row['id'] . "\")' > "; ?>
                                                 </div>
                                             </div>
                                         </div>
@@ -207,6 +207,118 @@ if (!isset($_SESSION['authAdmin'])) {
                 </form>
             </div>
         </dialog>
+
+
+
+        <!-- DELETE FOOD-ITEM MODAL -->
+        <dialog class="deleteFoodModal rounded" id="deleteFoodModal">
+            <div class="deleteFoodModalContentDiv d-flex flex-column justify-content-between align-items-center h-100">
+                <h1 class="deleteFoodHeaderMsg w-100 text-center h3" id="deleteFoodHeaderMsg">h</h1>
+                <hr class="w-100 p-0 m-0">
+                <img src="" alt="food-category" class="deleteFoodImg rounded">
+                <div class=" p-0 m-0 w-100 d-flex gap-3 align-items-center justify-content-center">
+                    <input type="submit" class="addTableConfirmBtn col-sm-5 p-2 fs-4 rounded" value="Yes" onclick="deleteFood()">
+                    <input type="submit" class="addTableCancelBtn col-sm-5 p-2 fs-4 rounded bg-secondary-subtle" value="No" onclick="deleteFoodModal.close()">
+                </div>
+            </div>
+        </dialog>
+
+
+        <!-- EDIT FOOD-ITEM MODAL -->
+        <dialog class="editFoodModal rounded p-3" id="editFoodModal">
+            <div class="editFoodModalContentDiv d-flex flex-column align-items-center justify-content-between h-100">
+                <h1 class="w-100 text-center display-6 fw-bold align-self-center p-0 m-0">Edit Food Item</h1>
+                <hr class="w-100 p-0 my-1">
+                <img src="" alt="food-category" class="editFoodModalImg rounded my-1" id="editFoodModalImg">
+                <form action="" method="post" enctype="multipart/form-data" class="d-flex flex-column align-items-center w-75">
+                    <input type="text" name="food_category_name" id="editFoodName" class="editFoodName my-2 col-sm-12 rounded py-2 px-4 fw-medium" placeholder="Enter Food category">
+                    <input type="text" name="food_category_name_compare" id="editFoodNameCompare" class="editFoodNameCompare" readonly hidden>
+                    <input type="text" name="food_category_id" id="editFoodId" class="editFoodId" readonly hidden>
+                    <?php $getFcList = "SELECT * FROM food_category ORDER BY category_name";
+                    $getFcListRes = mysqli_query($conn, $getFcList); ?>
+                    <select name="category_name" id="editFoodCategoryName" class="FcSelect col-sm-12 my-2 py-2 px-4 rounded" required>
+                        <?php if ($getFcListRes->num_rows > 0) { ?>
+                            <option value="" class="optionTxt w-100 col-sm-11" disabled hidden>Select Food Category</option>
+                            <?php while ($row = $getFcListRes->fetch_assoc()) { ?>
+                                <option class="optionTxt w-100"><?php echo $row['category_name']; ?></option>
+                            <?php } ?>
+                        <?php } ?>
+                    </select>
+                    <input type="file" name="food_category_image" id="editFoodImg" class="editFoodImg my-2 col-sm-12 rounded fw-medium">
+                    <input type="text" name="food_category_quantity" id="editFoodQuantity" class="editFoodQuantity my-2 col-sm-12 rounded py-2 px-4 fw-medium" placeholder="Enter Food Quantity">
+                    <input type="text" name="food_category_price" id="editFoodPrice" class="editFoodPrice my-2 col-sm-12 rounded py-2 px-4 fw-medium" placeholder="Enter Price" required>
+                    <div class="col col-sm-12 d-flex gap-3 mt-2 align-items-center justify-content-between p-0">
+                        <input type="submit" value="Edit Food Category" class="editFoodBtn w-50 py-2 fs-4 rounded" id="editFoodBtn">
+                        <input type="submit" value="Cancel" class="editFoodCancelBtn w-50 py-2 fs-4 rounded bg-secondary-subtle" id="editFoodCancelBtn" onclick="editFoodModal.close()">
+                    </div>
+                </form>
+            </div>
+        </dialog>
+
+
+
+        <!-- EDIT FOOD CATEGORY MODAL -->
+        <script>
+            // var editFcUrl;
+
+            function openEditFoodModal(editFoodImgData, editFoodName, editFoodID, editFoodCategory, editFoodQuantity, editFoodPrice) {
+                var editFoodModal = document.getElementById('editFoodModal');
+                var editFoodImg = editFoodModal.querySelector('.editFoodModalImg');
+                document.getElementById('editFoodName').value = editFoodName;
+                document.getElementById('editFoodId').value = editFoodID;
+                document.getElementById('editFoodNameCompare').value = editFoodName;
+                // document.getElementById('default_category').value = editFoodCategory;
+                document.getElementById('editFoodQuantity').value = editFoodQuantity;
+                document.getElementById('editFoodPrice').value = editFoodPrice;
+
+                // Set the default category value in the dropdown
+                var categoryDropdown = document.getElementById('editFoodCategoryName');
+                for (var i = 0; i < categoryDropdown.options.length; i++) {
+                    if (categoryDropdown.options[i].text === editFoodCategory) {
+                        categoryDropdown.options[i].selected = true;
+                        break;
+                    }
+                }
+
+                // Create a temporary canvas
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+
+                // Create a new image element
+                var image = new Image();
+
+                // Set the source of the new image to the QR image data
+                image.src = 'data:image;base64,' + editFoodImgData;
+
+                // Wait for the image to load
+                image.onload = function() {
+                    // Set the canvas size to match the image
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+
+                    // Draw the image onto the canvas
+                    context.drawImage(image, 0, 0);
+
+                    // Convert the canvas content to a blob with JPG format
+                    canvas.toBlob(function(blob) {
+                        // Create a new image element with the blob as the source
+                        var newImage = new Image();
+                        newImage.src = URL.createObjectURL(blob);
+
+                        // Set the source of the modal's image to the new image source
+                        editFoodImg.src = newImage.src;
+
+                        // Set the table name in the modal header
+                        // editFcName.value = editFcName;
+
+                        // editFcUrl = "./foodcategory.php?editFcId=" + editFcID;
+
+                        // Open the modal
+                        editFoodModal.showModal();
+                    }, 'image/jpeg', 1.0); // 1.0 means full quality
+                };
+            }
+        </script>
 
 
 
@@ -284,55 +396,121 @@ if (!isset($_SESSION['authAdmin'])) {
 
 
         <!-- ADD FOOD-ITEM -->
-        <?php
-        if (isset($_POST['food_name']) && isset($_FILES['food_image']) && isset($_POST['food_quantity']) && isset($_POST['food_price']) && isset($_POST['category_name'])) {
-            $FoodName = $_POST['food_name'];
+        <script>
+            <?php
+            if (isset($_POST['food_name']) && isset($_FILES['food_image']) && isset($_POST['food_quantity']) && isset($_POST['food_price']) && isset($_POST['category_name'])) {
+                $FoodName = $_POST['food_name'];
 
-            $FoodImageGet = $_FILES['food_image']['tmp_name'];
-            $name = addslashes($_FILES["food_image"]["tmp_name"]);
-            $FoodImageData = file_get_contents($FoodImageGet);
-            $FoodImage = base64_encode($FoodImageData);
+                $FoodImageGet = $_FILES['food_image']['tmp_name'];
+                $name = addslashes($_FILES["food_image"]["tmp_name"]);
+                $FoodImageData = file_get_contents($FoodImageGet);
+                $FoodImage = base64_encode($FoodImageData);
 
-            $FoodQuantity = $_POST['food_quantity'];
-            $FoodPrice = $_POST['food_price'];
-            $FoodCategoryName = $_POST['category_name'];
+                $FoodQuantity = $_POST['food_quantity'];
+                $FoodPrice = $_POST['food_price'];
+                $FoodCategoryName = $_POST['category_name'];
 
-            $checkIfFoodExists = "SELECT * FROM food_item WHERE food_name=? ";
-            $checkForFood = $conn->prepare($checkIfFoodExists);
-            $checkForFood->bind_param('s', $FoodName);
-            $checkForFood->execute();
-            $checkForFoodResult = $checkForFood->get_result();
+                $checkIfFoodExists = "SELECT * FROM food_item WHERE food_name=? ";
+                $checkForFood = $conn->prepare($checkIfFoodExists);
+                $checkForFood->bind_param('s', $FoodName);
+                $checkForFood->execute();
+                $checkForFoodResult = $checkForFood->get_result();
 
-            if ($checkForFoodResult->num_rows >= 1) {
-        ?>
-                <script>
+                if ($checkForFoodResult->num_rows >= 1) { ?>
                     window.location.href = "./fooditem.php";
-                </script>
-                <?php
-                $_SESSION['FoodAlreadyExists'] = "Food Item Already Exists...!";
-            } else {
-                $addFoodQuery = "INSERT INTO food_item(food_name, category_name, price, quantity, food_image) VALUES ('$FoodName', '$FoodCategoryName','$FoodPrice', '$FoodQuantity','$FoodImage') ";
-                $addFoodRun = mysqli_query($conn, $addFoodQuery);
-
-                if ($addFoodRun) {
-                ?>
-                    <script>
-                        window.location.href = "./fooditem.php";
-                    </script>
-                <?php
-                    $_SESSION["FoodAddedSuccessfully"] = "Food Item Added Successfully..!";
+                    <?php $_SESSION['FoodAlreadyExists'] = "Food Item Already Exists...!";
                 } else {
-                ?>
-                    <script>
+                    $addFoodQuery = "INSERT INTO food_item(food_name, category_name, price, quantity, food_image) VALUES ('$FoodName', '$FoodCategoryName','$FoodPrice', '$FoodQuantity','$FoodImage') ";
+                    $addFoodRun = mysqli_query($conn, $addFoodQuery);
+
+                    if ($addFoodRun) { ?>
                         window.location.href = "./fooditem.php";
-                    </script>
-        <?php
-                    $_SESSION["FoodAdditionFailure"] = "Failed to add Food Item..!";
+                    <?php $_SESSION["FoodAddedSuccessfully"] = "Food Item Added Successfully..!";
+                    } else { ?>
+                        window.location.href = "./fooditem.php";
+            <?php $_SESSION["FoodAdditionFailure"] = "Failed to add Food Item..!";
+                    }
                 }
             }
-        }
-        ?>
+            ?>
+        </script>
 
+
+
+        <!-- DELETE FOOD CATEGORY MODAL -->
+        <script>
+            var deleteFoodUrl;
+
+            function openDeleteFoodModal(deleteFoodImgData, deleteFoodName, deleteFoodID) {
+                var deleteFoodModal = document.getElementById('deleteFoodModal');
+                var deleteFoodImg = deleteFoodModal.querySelector('.deleteFoodImg');
+                var deleteFoodHeaderMsg = deleteFoodModal.querySelector('#deleteFoodHeaderMsg');
+
+                // Create a temporary canvas
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+
+                // Create a new image element
+                var image = new Image();
+
+                // Set the source of the new image to the QR image data
+                image.src = 'data:image;base64,' + deleteFoodImgData;
+
+                // Wait for the image to load
+                image.onload = function() {
+                    // Set the canvas size to match the image
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+
+                    // Draw the image onto the canvas
+                    context.drawImage(image, 0, 0);
+
+                    // Convert the canvas content to a blob with JPG format
+                    canvas.toBlob(function(blob) {
+                        // Create a new image element with the blob as the source
+                        var newImage = new Image();
+                        newImage.src = URL.createObjectURL(blob);
+
+                        // Set the source of the modal's image to the new image source
+                        deleteFoodImg.src = newImage.src;
+
+                        // Set the table name in the modal header
+                        deleteFoodHeaderMsg.textContent = "Are you sure you want to delete '" + deleteFoodName + "'?";
+
+                        deleteFoodUrl = "./fooditem.php?deleteFoodId=" + deleteFoodID;
+
+                        // Open the modal
+                        deleteFoodModal.showModal();
+                    }, 'image/jpeg', 1.0); // 1.0 means full quality
+                };
+            }
+
+
+            function deleteFood() {
+                window.location.href = deleteFoodUrl;
+                // console.log(deleteFoodUrl);
+            }
+        </script>
+
+
+
+        <!-- DELETE FOOD-ITEM -->
+        <script>
+            <?php
+            if (isset($_GET['deleteFoodId'])) {
+                $deleteFoodId = $_GET['deleteFoodId'];
+                $deleteFoodQuery = "DELETE FROM food_item WHERE id='$deleteFoodId' ";
+                $deleteFoodRun = mysqli_query($conn, $deleteFoodQuery);
+                if ($deleteFoodRun) { ?>
+                    window.location.href = "./fooditem.php";
+                <?php $_SESSION["FoodDeletionSuccess"] = "Food Category Deleted Successfully..!";
+                } else { ?>
+                    window.location.href = "./fooditem.php";
+            <?php $_SESSION["FoodDeletionFailure"] = "Failed to delete Food Category..!";
+                }
+            }
+            ?>
+        </script>
 </body>
 
 </html>
