6e5d0a7c304b8a9184297610a3cd13cdc6ee07c8
Thej48 <chattangadathimmaiah@gmail.com>
2024-01-29 23:26:37 +0530

    18th commit : working on user end sign in functionality

--- admin/adminProfile.php
+++ admin/adminProfile.php
@@ -196,7 +196,7 @@ if (!isset($_SESSION['authAdmin'])) {
                                                 <?php } else { ?>
                                                     <?php echo "
                                                     <input type='submit' value='Edit' class='editAdminBtn w-50 rounded py-1 fw-medium' id='editAdminBtn' onClick='openEditAdminModal(\"" . $data['admin_photo'] . "\", \"" . $data['admin_name'] . "\", \"" . $data['id'] . "\", \"" . $data['admin_email'] . "\", \"" . $data['admin_phone'] . "\")'>
-                                                    <input type='submit' value='Delete' class='deleteAdminBtn bg-secondary-subtle w-50 rounded py-1 fw-bold' id='deleteAdminBtn'> "; ?>
+                                                    <input type='submit' value='Delete' class='deleteAdminBtn bg-secondary-subtle w-50 rounded py-1 fw-bold' id='deleteAdminBtn' onClick='openDeleteAdminModal(\"" . $data['admin_photo'] . "\", \"" . $data['admin_name'] . "\", \"" . $data['id'] . "\")'> "; ?>
                                                 <?php } ?>
                                             <?php } elseif ($row['role'] == 'Admin') { ?>
                                                 <?php if ($data['admin_id'] == $admin_id) { ?>
@@ -261,14 +261,14 @@ if (!isset($_SESSION['authAdmin'])) {
         <div class="editAdminModalContentDiv h-100 d-flex flex-column p-0 m-0 align-self-center align-items-center justify-content-center">
             <h1 class="m-0 p-0 display-6 fw-bold">Add Admin</h1>
             <hr class="w-100 my-2">
-            <img src="" alt="admin-image" class="adminImgEditModal">
+            <img src="" alt="admin-image" class="adminImgEditModal rounded">
             <form action="./adminProfile.php" method="post" enctype="multipart/form-data" class="d-flex flex-column align-items-center w-75">
                 <!-- <input type="text" name="admin_id" placeholder="Enter Admin ID" id="admin_id" class="admin_id col-sm-11 my-2 py-2 px-4 rounded" required readonly> -->
-                <input type="text" name="edit_admin_id" placeholder="Enter Admin ID" id="edit_admin_id" class="edit_admin_id col-sm-11 my-2 py-2 px-4 rounded" required>
+                <input type="text" name="edit_admin_id" placeholder="Enter Admin ID" id="edit_admin_id" class="edit_admin_id col-sm-11 my-2 py-2 px-4 rounded" readonly hidden required>
                 <input type="text" name="edit_admin_name" placeholder="Enter Admin Name" id="edit_admin_name" class="edit_admin_name col-sm-11 my-2 py-2 px-4 rounded" required>
                 <input type="text" name="edit_admin_email" placeholder="Enter Email ID" id="edit_admin_email" class="edit_admin_email col-sm-11 my-2 py-2 px-4 rounded" required>
                 <input type="text" name="edit_admin_phone" placeholder="Enter Phone Number" id="edit_admin_phone" class="edit_admin_phone col-sm-11 my-2 py-2 px-4 rounded" required>
-                <input type="file" name="edit_admin_photo" id="edit_admin_photo" class="col-sm-11 my-2 rounded edit_admin_photo" required>
+                <input type="file" name="edit_admin_photo" id="edit_admin_photo" class="col-sm-11 my-2 rounded edit_admin_photo">
                 <!-- <input type="text" name="admin_password" placeholder="Enter Password" id="admin_password" class="admin_password col-sm-11 my-2 py-2 px-4 rounded" required> -->
                 <div class="editAdminControlDiv col-sm-11 d-flex align-items-center justify-content-center gap-3 mt-2">
                     <input type="submit" value="Add Admin" class="editAdminConfirmBtn w-50 py-2 fs-4 rounded">
@@ -277,6 +277,99 @@ if (!isset($_SESSION['authAdmin'])) {
             </form>
         </div>
     </dialog>
+
+
+
+    <!-- DELETE ADMIN MODAL -->
+    <dialog class="deleteAdminModal rounded" id="deleteAdminModal">
+        <div class="deleteAdminModalContentDiv d-flex flex-column justify-content-between align-items-center h-100">
+            <h1 class="deleteAdminHeaderMsg w-100 text-center h3" id="deleteAdminHeaderMsg">h</h1>
+            <hr class="w-100 p-0 m-0">
+            <img src="" alt="food-category" class="deleteAdminImg rounded">
+            <div class=" p-0 m-0 w-100 d-flex gap-3 align-items-center justify-content-center">
+                <input type="submit" class="addTableConfirmBtn col-sm-5 p-2 fs-4 rounded" value="Yes" onclick="deleteAdmin()">
+                <input type="submit" class="addTableCancelBtn col-sm-5 p-2 fs-4 rounded bg-secondary-subtle" value="No" onclick="deleteAdminModal.close()">
+            </div>
+        </div>
+    </dialog>
+
+
+
+
+    <!-- DELETE FOOD CATEGORY MODAL -->
+    <script>
+        var deleteAdminURL;
+
+        function openDeleteAdminModal(deleteAdminImgData, deleteAdminName, deleteAdminID) {
+            var deleteAdminModal = document.getElementById('deleteAdminModal');
+            var deleteAdminImg = deleteAdminModal.querySelector('.deleteAdminImg');
+            var deleteAdminHeaderMsg = deleteAdminModal.querySelector('#deleteAdminHeaderMsg');
+
+            // Create a temporary canvas
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+
+            // Create a new image element
+            var image = new Image();
+
+            // Set the source of the new image to the QR image data
+            image.src = 'data:image;base64,' + deleteAdminImgData;
+
+            // Wait for the image to load
+            image.onload = function() {
+                // Set the canvas size to match the image
+                canvas.width = image.width;
+                canvas.height = image.height;
+
+                // Draw the image onto the canvas
+                context.drawImage(image, 0, 0);
+
+                // Convert the canvas content to a blob with JPG format
+                canvas.toBlob(function(blob) {
+                    // Create a new image element with the blob as the source
+                    var newImage = new Image();
+                    newImage.src = URL.createObjectURL(blob);
+
+                    // Set the source of the modal's image to the new image source
+                    deleteAdminImg.src = newImage.src;
+
+                    // Set the table name in the modal header
+                    deleteAdminHeaderMsg.textContent = "Are you sure you want to delete '" + deleteAdminName + "'?";
+
+                    deleteAdminURL = "./adminProfile.php?deleteAdminId=" + deleteAdminID;
+
+                    // Open the modal
+                    deleteAdminModal.showModal();
+                }, 'image/jpeg', 1.0); // 1.0 means full quality
+            };
+        }
+
+
+        function deleteAdmin() {
+            window.location.href = deleteAdminURL;
+            // console.log(deleteAdminURL);
+        }
+    </script>
+
+
+
+    <!-- DELETE FOOD-ITEM -->
+    <script>
+        <?php
+        if (isset($_GET['deleteAdminId'])) {
+            $deleteAdminId = $_GET['deleteAdminId'];
+            $deleteAdminQuery = "DELETE FROM admin WHERE id='$deleteAdminId' ";
+            $deleteAdminRun = mysqli_query($conn, $deleteAdminQuery);
+            if ($deleteAdminRun) { ?>
+                window.location.href = "./adminProfile.php";
+            <?php $_SESSION["FoodDeletionSuccess"] = "Food Category Deleted Successfully..!";
+            } else { ?>
+                window.location.href = "./adminProfile.php";
+        <?php $_SESSION["FoodDeletionFailure"] = "Failed to delete Food Category..!";
+            }
+        }
+        ?>
+    </script>
 
 
 
@@ -318,16 +411,82 @@ if (!isset($_SESSION['authAdmin'])) {
                     // Set the source of the modal's image to the new image source
                     editAdminImg.src = newImage.src;
 
-                    // Set the table name in the modal header
-                    // editFcName.value = editFcName;
-
-                    // editFcUrl = "./foodcategory.php?editFcId=" + editFcID;
-
-                    // Open the modal
                     editAdminModal.showModal();
                 }, 'image/jpeg', 1.0); // 1.0 means full quality
             };
         }
+    </script>
+
+
+
+    <!-- EDIT ADMIN DATA -->
+    <script>
+        <?php
+        if (isset($_POST['edit_admin_id'])) {
+            $editAdminId = $_POST['edit_admin_id'];
+
+            // EDIT WITHOUT IMAGE
+            if (
+                isset($_POST['edit_admin_name'])
+                && isset($_POST['edit_admin_email'])
+                && isset($_POST['edit_admin_phone'])
+            ) {
+                $editAdminName = $_POST['edit_admin_name'];
+                $editAdminEmail = $_POST['edit_admin_email'];
+                $editAdminPhone = $_POST['edit_admin_phone'];
+
+
+                $editAdminQuery = "UPDATE admin SET admin_name='$editAdminName', admin_email='$editAdminEmail', admin_phone='$editAdminPhone' WHERE id='$editAdminId' ";
+                $editAdminRun = mysqli_query($conn, $editAdminQuery);
+
+                if ($editAdminRun) {
+        ?>
+                    window.location.href = "./adminProfile.php";
+                <?php
+                    $_SESSION["FoodDetailsUpdated"] = "Food Item Updated Successfully..!";
+                } else {
+                ?>
+                    window.location.href = "./adminProfile.php";
+                    <?php
+                    $_SESSION["FoodDetailsUpdationFailed"] = "Failed to Update Food Item..!";
+                }
+            }
+
+            // EDIT WITH IMAGE
+            if (
+                isset($_POST['edit_admin_name'])
+                && isset($_POST['edit_admin_email'])
+                && isset($_POST['edit_admin_phone'])
+                && isset($_FILES['edit_admin_photo'])
+            ) {
+                $editAdminName = $_POST['edit_admin_name'];
+                $editAdminEmail = $_POST['edit_admin_email'];
+                $editAdminPhone = $_POST['edit_admin_phone'];
+                $adminImageData = $_FILES['edit_admin_photo'];
+                if ($_FILES['edit_admin_photo']['error'] == UPLOAD_ERR_OK) {
+                    $editAdminImageGet = $_FILES['edit_admin_photo']['tmp_name'];
+                    $editAdminImageData = file_get_contents($editAdminImageGet);
+                    $editAdminImage = base64_encode($editAdminImageData);
+
+                    $editAdminQuery = "UPDATE admin SET admin_name='$editAdminName', admin_email='$editAdminEmail', admin_phone='$editAdminPhone' , admin_photo='$editAdminImage' WHERE id='$editAdminId' ";
+                    $editAdminRun = mysqli_query($conn, $editAdminQuery);
+
+                    if ($editAdminRun) {
+                    ?>
+                        window.location.href = "./adminProfile.php";
+                    <?php
+                        $_SESSION["FoodDetailsUpdated"] = "Food Item Updated Successfully..!";
+                    } else {
+                    ?>
+                        window.location.href = "./adminProfile.php";
+        <?php
+                        $_SESSION["FoodDetailsUpdationFailed"] = "Failed to Update Food Item..!";
+                    }
+                }
+            }
+        }
+
+        ?>
     </script>
 
 
